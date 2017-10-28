79e29c50e57c4206e6cb904b5f45f39e9c683a76
Alasdair Mercer <mercer.alasdair@gmail.com>
2017-10-28 22:58:50 +0100

    Revert "Added new scale option to API & CLI"

--- src/index.js
+++ src/index.js
@@ -22,4 +22,58 @@
 
 'use strict';
 
-module.exports = require('./converter');
+const Converter = require('./converter');
+const { version } = require('../package.json');
+
+/**
+ * Converts the specified <code>source</code> SVG into a PNG using the <code>options</code> provided via a headless
+ * Chromium instance.
+ *
+ * <code>source</code> can either be a SVG buffer or string.
+ *
+ * If the width and/or height cannot be derived from <code>source</code> then they must be provided via their
+ * corresponding options. This method attempts to derive the dimensions from <code>source</code> via any
+ * <code>width</code>/<code>height</code> attributes or its calculated <code>viewBox</code> attribute.
+ *
+ * An error will occur if <code>source</code> does not contain an SVG element or no <code>width</code> and/or
+ * <code>height</code> options were provided and this information could not be derived from <code>source</code>.
+ *
+ * @param {Buffer|string} source - the SVG source to be converted to a PNG
+ * @param {Converter~ConvertOptions} [options] - the options to be used
+ * @return {Promise.<Buffer, Error>} A <code>Promise</code> for the asynchronous conversion that is resolved with the
+ * PNG buffer.
+ * @public
+ */
+async function convert(source, options) {
+  // Reference method via module.exports to allow unit test to spy on converter
+  const converter = module.exports.createConverter();
+  let target;
+
+  try {
+    target = await converter.convert(source, options);
+  } finally {
+    await converter.destroy();
+  }
+
+  return target;
+}
+
+/**
+ * Creates an instance of {@link Converter}.
+ *
+ * It is important to note that, after the first time {@link Converter#convert} is called, a headless Chromium instance
+ * will remain open until {@link Converter#destroy} is called. This is done automatically when using the main API
+ * <code>convert</code> method within this module, however, when using {@link Converter} directly, it is the
+ * responsibility of the caller. Due to the fact that creating browser instances is expensive, this level of control
+ * allows callers to reuse a browser for multiple conversions. For example; one could create a {@link Converter} and use
+ * it to convert a collection of SVG files to PNG files and then destroy it afterwards. It's not recommended to keep an
+ * instance around for too long, as it will use up resources.
+ *
+ * @return {Converter} A newly created {@link Converter} instance.
+ * @public
+ */
+function createConverter() {
+  return new Converter();
+}
+
+module.exports = { convert, createConverter, version };
